f6c995b646972176c5c059061105ab202a1ddc54
Gabriel Gonçalves <gabriel.ggoncalves@linx.com.br>
2023-03-12 15:36:40 -0300

    refatorando espaçamento dos templates

--- alura-tracker/src/hooks/notificador.ts
+++ alura-tracker/src/hooks/notificador.ts
@@ -2,14 +2,12 @@ import { TipoNotificao } from "@/Interfaces/INotificacao"
 import { store } from "@/store"
 import { NOTIFICAR } from "@/store/tipo-multacoes"
 
-
 // reaproveitamento de códito qe podera ser utilizado em outras telas 
 type Notificador = {
     notificar: (titulo: string, texto: string, tipo: TipoNotificao) => void
 }
 
 export default () : Notificador => {
-
     const notificar = (titulo: string, texto: string, tipo: TipoNotificao) : void => {
         store.commit(NOTIFICAR, {
             titulo,
@@ -17,7 +15,6 @@ export default () : Notificador => {
             tipo
         })
     }
-
     return {
         notificar
     }
